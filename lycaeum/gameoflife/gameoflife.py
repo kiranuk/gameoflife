c9680649ee1998206c38da915e13009ee065103c
kiranuk <Kiran6475@icloud.com>
2019-02-24 17:43:34 +0530

    game not working

--- lycaeum/gameoflife/gameoflife.py
+++ lycaeum/gameoflife/gameoflife.py
@@ -1,8 +1,12 @@
 import numpy as np
 
-def alive(cell,theboard):
-    if cell == 1:
-        return True
+def alive(theboard, row, col):
+    for row in range(len(theboard)):
+        for col in range(len(theboard[0])):
+            if theboard[row][col] == 1:
+                return True
+            else:
+                return False
 
 
 def neibhors(theboard,n=3):
@@ -19,7 +23,7 @@ def neibhors(theboard,n=3):
                     result = (theboard[i-1][j], theboard[i][j], theboard[i][j+1]).count(1)
                     neibhors_count[i-1][j+1] = result
                     continue
-                if j < n:
+                if j == 1:
                     result = (theboard[i-1][j-1], theboard[i][j-1], theboard[i][j], theboard[i][j+1], theboard[i-1][j+1]).count(1)
                     neibhors_count[i-1][j] = result
                     continue
@@ -77,7 +81,7 @@ def display(theboard):
 
 
 def main():
-    theboard = [[1,0,0], [0,1,0], [0,0,1]]
+    theboard = np.random.randint(2,size=(3, 3))
     print(theboard)
     print(neibhors(theboard))
     display(theboard)
--- lycaeum/gameoflife/test_game_of_life.py
+++ lycaeum/gameoflife/test_game_of_life.py
@@ -1,10 +1,13 @@
 import gameoflife
 
-
+def test_alive():
+    theboard = [[1,0,0],[0,1,0],[0,0,1]]
+    assert (gameoflife.alive(theboard, 0, 0) == True)
+    assert (gameoflife.alive(theboard, 1, 1) == True)
 def test_neibhors():
-    theboard = [[True,False,False], [False,True,False], [False,False,True]]
+    theboard = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
     result = [[1, 2, 1], [2, 2, 2], [1, 2, 1]]
-    assert (gameoflife.neibhors(theboard) == result)
+    assert (gameoflife.neibhors(theboard, 3) == result for _ in range(len(theboard)))
 def test_rules():
     actual = [1,2,1]
     result = [2,2,2]
