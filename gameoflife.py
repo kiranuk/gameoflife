33baa494e80bf35d5810c2cc7f182227c30d8b97
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-27 16:03:01 +0530

    Fixes typo.
    Don't leave them in the code.

--- gameoflife.py
+++ gameoflife.py
@@ -4,24 +4,24 @@ def alive(cell):
     return cell == 1
 
 
-def neibhors(theboard):
+def neighbours(theboard):
     n = len(theboard)
-    neibhors_count = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    neighbours_count = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     for i in range(len(theboard)):
         for j in range(len(theboard[0])):
             
             if i == 0:
                 if j == 0:
                     result = (theboard[0][1], theboard[1][0], theboard[1][1]).count(1)
-                    neibhors_count[0][0] = result
+                    neighbours_count[0][0] = result
                     continue
                 if j == 1:
                     result = (theboard[0][0], theboard[1][0], theboard[1][1], theboard[1][2], theboard[0][2]).count(1)
-                    neibhors_count[0][1] = result
+                    neighbours_count[0][1] = result
                     continue
                 if j == 2:
                     result = (theboard[0][1], theboard[1][1], theboard[1][2]).count(1)
-                    neibhors_count[0][2] = result
+                    neighbours_count[0][2] = result
                     continue
 
 
@@ -29,44 +29,44 @@ def neibhors(theboard):
             if i == 1:
                 if j == 0:
                     result = (theboard[0][0], theboard[0][1], theboard[1][1], theboard[2][1], theboard[2][0]).count(1)
-                    neibhors_count[1][0] = result
+                    neighbours_count[1][0] = result
                     continue
                 if j == 1:
                     result = (theboard[0][0], theboard[0][1], theboard[0][2], theboard[1][0], theboard[1][2], theboard[2][0],theboard[2][1], theboard[2][2]).count(1)
-                    neibhors_count[1][1] = result
+                    neighbours_count[1][1] = result
                     continue
                 if j == 2:
                     result = (theboard[0][2], theboard[0][1], theboard[1][1], theboard[2][1], theboard[2][2]).count(1)
-                    neibhors_count[1][2] = result
+                    neighbours_count[1][2] = result
                     continue
 
             
             if i == 2:
                 if j == 0:
                     result = (theboard[1][0], theboard[1][1], theboard[2][1]).count(1)
-                    neibhors_count[2][0] = result
+                    neighbours_count[2][0] = result
                     continue
                 if j == 2:
                     result = (theboard[1][2], theboard[1][1], theboard[2][1]).count(1)
-                    neibhors_count[2][2] = result
+                    neighbours_count[2][2] = result
                     continue
                 if j ==1:
                     result = (theboard[2][0], theboard[1][0], theboard[1][1], theboard[1][2], theboard[2][2]).count(1)
-                    neibhors_count[2][1] = result
+                    neighbours_count[2][1] = result
                     continue
-    return neibhors_count
+    return neighbours_count
 
 
-def rules(theboard, neibhors):
-    rows = len(neibhors)
-    cols = len(neibhors[0])
+def rules(theboard, neighbours):
+    rows = len(neighbours)
+    cols = len(neighbours[0])
     for i in range(rows):
         for j in range(cols):             
-            if neibhors[i][j] not in range(2,4):
+            if neighbours[i][j] not in range(2,4):
                 theboard[i][j] = 0
                 continue
                 
-            if neibhors[i][j] == 3:
+            if neighbours[i][j] == 3:
                 theboard[i][j] = 1
                 continue
     return theboard
@@ -91,7 +91,7 @@ def main(theboard):
         print ("{} generation".format(i))
         time.sleep(0.5)
         print(display(theboard))
-        rules(theboard, neibhors(theboard))
+        rules(theboard, neighbours(theboard))
 
 
 
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -15,18 +15,18 @@ def test_alive():
 
 
 
-def test_neibhors():
+def test_neighbours():
     theboard = [[1, 0, 0],
                 [0, 1, 0],
                 [0, 0, 1]]
     result = [[1, 2, 1],
               [2, 2, 2],
               [1, 2, 1]]
-    assert (gameoflife.neibhors(theboard) == result)
+    assert (gameoflife.neighbours(theboard) == result)
 
 def test_rules():
     theboard = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
-    neibhors = [[2, 3, 2], [1, 2, 1], [2, 3, 2]]
+    neighbours = [[2, 3, 2], [1, 2, 1], [2, 3, 2]]
     result = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
-    assert (gameoflife.rules(theboard,neibhors) == result) 
+    assert (gameoflife.rules(theboard,neighbours) == result) 
 
