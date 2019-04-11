edc0b007c3ec4009a805991a3d86e966858a7e0b
kiranuk <Kiran6475@icloud.com>
2019-04-11 18:30:31 +0530

    Change function name to neighbours() and adds display() to test

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -7,23 +7,30 @@ def test_alive():
     result = [[True,False,False],
               [False,True,False],
               [False,False,True]]
-    assert (gameoflife.alive(theboard[0][0]) == result[0][0])
-    assert (gameoflife.alive(theboard[1][0]) == result[1][0])
-    assert (gameoflife.alive(theboard[2][0]) == result[2][0])
-    assert (gameoflife.alive(theboard[1][1]) == result[1][1])
-    assert (gameoflife.alive(theboard[2][1]) == result[2][1])
+    assert gameoflife.alive(theboard[0][0]) == result[0][0]
+    assert gameoflife.alive(theboard[1][0]) == result[1][0]
+    assert gameoflife.alive(theboard[2][0]) == result[2][0]
+    assert gameoflife.alive(theboard[1][1]) == result[1][1]
+    assert gameoflife.alive(theboard[2][1]) == result[2][1]
 
 
 
-def test_neibhours():
-    theboard = [[0, 0, 0],
-                [1, 1, 1],
-                [0, 0, 0]]
-    result = 0
-    assert (gameoflife.neibhours(theboard, 3, 3) == result)
+def test_neighbours():
+    theboard = [[0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0]]
+
+    assert gameoflife.neighbours(theboard, 0, 0)== 2
+    assert gameoflife.neighbours(theboard, 1, 1)== 2
+    assert gameoflife.neighbours(theboard, 2, 1)== 1    
 
 def test_rules():
     theboard = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
     result = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
-    assert (gameoflife.rules(theboard) == result) 
+    assert gameoflife.rules(theboard) == result
 
+def test_display():
+    size = 3
+    theboard = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+    result = '\u25E6 \u25E6 \u25E6\n\n\u25B2 \u25B2 \u25B2\n\n\u25E6 \u25E6 \u25E6'
+    assert gameoflife.display(theboard) == result
