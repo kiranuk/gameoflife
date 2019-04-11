9767c1ee8ea497374b1a66157c8d95bbefe2f612
kiranuk <Kiran6475@icloud.com>
2019-04-11 21:53:06 +0530

    Refactors

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,12 +1,15 @@
+"Import gameoflife to test"
 import gameoflife
 
+
 def test_alive():
-    theboard = [[1,0,0],
-                [0,1,0],
-                [0,0,1]]
-    result = [[True,False,False],
-              [False,True,False],
-              [False,False,True]]
+    "Checking for alive cell"
+    theboard = [[1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1]]
+    result = [[True, False, False],
+              [False, True, False],
+              [False, False, True]]
     assert gameoflife.alive(theboard[0][0]) == result[0][0]
     assert gameoflife.alive(theboard[1][0]) == result[1][0]
     assert gameoflife.alive(theboard[2][0]) == result[2][0]
@@ -14,23 +17,25 @@ def test_alive():
     assert gameoflife.alive(theboard[2][1]) == result[2][1]
 
 
-
 def test_neighbours():
+    "Checking for alive neighbours"
     theboard = [[0, 1, 0],
                 [0, 1, 0],
                 [0, 1, 0]]
+    assert gameoflife.neighbours(theboard, 0, 0) == 2
+    assert gameoflife.neighbours(theboard, 1, 1) == 2
+    assert gameoflife.neighbours(theboard, 2, 1) == 1
 
-    assert gameoflife.neighbours(theboard, 0, 0)== 2
-    assert gameoflife.neighbours(theboard, 1, 1)== 2
-    assert gameoflife.neighbours(theboard, 2, 1)== 1    
 
 def test_rules():
+    "Checking for rules are correctly applied"
     theboard = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
     result = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
     assert gameoflife.rules(theboard) == result
 
+
 def test_display():
-    size = 3
+    "Checking display with given characters"
     theboard = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
     result = '\u25E6 \u25E6 \u25E6\n\n\u25B2 \u25B2 \u25B2\n\n\u25E6 \u25E6 \u25E6'
     assert gameoflife.display(theboard) == result
